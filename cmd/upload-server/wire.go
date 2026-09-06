b8f386b2748735e5deb8560c4d48b647cdd9c656
tkdn <tkdnation@gmail.com>
2026-09-06 19:47:12 +0900

    chore: tweak code and vscode settings

--- cmd/upload-server/wire.go
+++ cmd/upload-server/wire.go
@@ -20,13 +20,13 @@ type serverDeps struct {
 func initializeUploadServerDeps(s3Client *s3.Client, urlSigner *sign.URLSigner, cloudfrontDomain string, expires time.Duration) serverDeps {
 	wire.Build(
 		wire.Struct(new(serverDeps), "*"),
-		newRealS3Adapter,
 		wire.Bind(new(s3PostPolicyPresigner), new(*realS3Adapter)),
 		wire.Bind(new(s3ObjectHeadChecker), new(*realS3Adapter)),
-		newRealCloudFrontSigner,
 		wire.Bind(new(cloudFrontSigner), new(*realCloudFrontSigner)),
-		newMemoryAssetStore,
 		wire.Bind(new(assetStore), new(*memoryAssetStore)),
+		newRealS3Adapter,
+		newRealCloudFrontSigner,
+		newMemoryAssetStore,
 	)
 	return serverDeps{}
 }
